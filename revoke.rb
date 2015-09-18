c4d501dc61675605c3ed3add12039d5f51c6b4ed
Ash Wilson <smashwilson@gmail.com>
2015-09-18 10:24:59 -0400

    Implement revoke utility.

--- revoke.rb
+++ revoke.rb
@@ -1,4 +1,9 @@
 #!/bin/env ruby
 
-KEYS=%w{
-}
+require 'httparty'
+
+ARGV.each do |key|
+  HTTParty.delete "https://developer.rackspace.com:9000/keys/#{key}",
+    headers: { "Authorization" => "deconst apikey=\"#{ENV['ADMIN_APIKEY']}\"" }
+  puts "Key [#{key}] revoked."
+end
