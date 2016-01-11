06d6bcc25eefd554827a1774f6c4ef20e10ea5c0
Ash Wilson <smashwilson@gmail.com>
2016-01-11 15:48:07 -0500

    Clarify what's required and optional.

--- env.example.sh
+++ env.example.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Optional
+export SLACK_TOKEN=
+
+# Specify *one* of the following
 # export ADMIN_APIKEY=
 export DECONST_APIKEY=
 
+# Required: GitHub personal access token. Generate at https://github.com/settings/tokens
 export GITHUB_TOKEN=
-export SLACK_TOKEN=
